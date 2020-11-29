effc8aacb50b66853c723be0f35576a4f0cfd7b2
fk652 <fk652@nyu.edu>
2020-11-28 22:28:29 -0500

    updated datasets and milestones

---
+++ datasets/Yahoo Finance Stock Data/spark_calculations/run_all_jobs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+shopt -s expand_aliases
+source /home/fk652/.bashrc
+
+cd 'S&P_500'
+./spark_calculations.sh
+./spark_day_drops_gains.sh
+
+cd ../'Sectoral'
+./spark_calculations.sh
+./spark_day_drops_gains.sh
+
+cd ../'S&P_500_aggregate'
+./spark_calculations.sh
