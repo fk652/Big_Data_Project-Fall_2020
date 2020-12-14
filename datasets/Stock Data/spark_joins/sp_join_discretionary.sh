3d0d386a544722e7f6ad1a45115cd70d3ef5d39a
Istvan212 <ndarvas@gmail.com>
2020-12-14 11:10:23 -0500

    update

--- datasets/Stock Data/spark_joins/sp_join_discretionary.sh
+++ datasets/Stock Data/spark_joins/sp_join_discretionary.sh
@@ -1,29 +1,32 @@
+#shopt -s expand_aliases
+#source /home/fk652/.bashrc
 
 . /etc/profile.d/modules.sh
 # load up necessary modules
+
 echo "loading modules"
 module load python/gnu/3.6.5 #load python version
 module load spark/2.4.0 #load spark version
 
 # remove existing output files
 echo "removing existing output files"
-#hfs -rm -r 'spjoin_discretionary.out'
-#rm -r 'spjoin_discretionary.csv'
+hfs -rm -r 'spjoin_sectors.out'
+rm -r 'spjoin_sectors.csv'
 
 # uploading data to hadoop
-#echo "uploading data to hadoop"
-#hfs -rm -r 'Sectoral_stock_data.csv'
+echo "uploading data to hadoop"
+hfs -rm -r 'Sectoral_stock_data.csv'
 #hfs -put ../../'Sectoral_stock_data.csv'
 
 # run spark job
 echo "running spark job"
 spark-submit --conf \
 spark.pyspark.python=/share/apps/python/3.6.5/bin/python \
-sp_join_discretionary.py SnP_500_stock_data.csv \
-SnP_500_Information.csv
+sp_join_sectors.py "S&P_500_stock_data.csv" \
+"S&P_500_Information.csv"
 
 # retrieve output from Hadoop
 echo "retrieving hadoop output"
-hfs -getmerge 'spjoin_discretionary.out' 'spjoin_discretionary.csv'
+hfs -getmerge 'spjoin_sectors.out' 'spjoin_sectors.csv'
 
 echo "done"
